cd092084584cf717b8919378aa068ca5dba2745e
eric6166 <ericnguyen6166@gmail.com>
2024-04-25 16:16:25 +0700

    commit

--- src/main/java/com/eric6166/inventory/InventoryServiceApplication.java
+++ src/main/java/com/eric6166/inventory/InventoryServiceApplication.java
@@ -2,10 +2,12 @@ package com.eric6166.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.eric6166" })
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
